6d23de0820bb94ff7bcaafea2079055b911bc9e0
Ricardo Ruiz <rjruizfdez@gmail.com>
2017-08-07 00:04:23 +0200

    Initial version - ID of a user

---
+++ public/main.js
@@ -0,0 +1,31 @@
+angular.module('angularInstagram', []);  
+
+function mainController($scope, $http) {  
+
+    $scope.username = "ricardojoserf";
+    // $scope.password = "passqwe12qeq";
+    $scope.result = "";
+    
+    $scope.submit = function(){
+		
+        /*
+        $http({
+            url: "/exec", 
+            method: "GET",
+            params: {username: $scope.username, password: $scope.password }
+         });*/
+  
+        $http.get('/exec', { params:  {username: $scope.username, password: $scope.password } } )
+            .success(
+            	function(response) {
+            		$scope.result = response;
+            	}
+            )
+            .error(
+            	function() {
+                	console.log('Error');
+            	}   
+            );
+    };
+
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+var express     = require('express');  
+var app         = express();  
+var exec        = require('child_process').exec;
+ 
+
+app.configure(function() {  
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.logger('dev')); 
+    app.use(express.bodyParser());              
+    app.use(express.methodOverride());                  
+});
+
+app.get("/exec", function (req, res) {  
+    
+    var name = req.query['username'];
+    var pass = req.query['password'];
+
+    var command = "python /home/ricardo/Projects/instagram-user-id/main.py "+name;
+    
+    exec(command, function (error, stdout, stderr) {
+        if (error === null) {
+            res.header("Content-Type", "application/json");
+            //res.end(JSON.stringify(stdout, null, "\t"), "utf-8");
+            res.end(stdout);
+        }
+    });
+
+});
+
+app.get('*', function(req, res) {  
+    res.sendfile('./public/index.html');
+});
+
+app.listen(8080, function() {  
+    console.log('Visit... http://localhost:8080/');
+});
